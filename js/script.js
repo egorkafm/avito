4adb2e62b23b540279dfe7e425ee6eb4fef6b673
Yehor Lashchenko <egor_and1@i.ua>
2020-07-09 16:26:03 +0300

    add modal windows

---
+++ js/script.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const dataBase = [];
+
+const modalAdd = document.querySelector('.modal__add'),
+	addAd = document.querySelector('.add__ad'),
+	modalBtnSubmit = document.querySelector('.modal__btn-submit'),
+	modalSubmit = document.querySelector('.modal__submit'),
+	catalog = document.querySelector('.catalog'),
+	modalItem = document.querySelector('.modal__item'),
+	modalBtnWarning = document.querySelector('.modal__btn-warning');
+
+const elementsModalSubmit = [...modalSubmit.elements]
+	.filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit');
+		
+
+
+const closeModal = function(event) {
+	const target = event.target;
+	if (target.closest('.modal__close') || target === this) {
+		this.classList.add('hide');
+		if (this === modalAdd) {
+			modalSubmit.reset();
+		};
+	};
+};
+
+const closeModalEsc = event => {
+	if (event.code === 'Escape') {
+		modalAdd.classList.add('hide');
+		modalItem.classList.add('hide');
+		modalSubmit.reset();
+		document.removeEventListener('keydown', closeModalEsc);
+	};
+};
+
+modalSubmit.addEventListener('input', () => {
+	const validForm = elementsModalSubmit.every(elem => elem.value);
+	modalBtnSubmit.disabled = !validForm;
+	modalBtnWarning.style.display = validForm ? 'none' : '';
+});
+
+modalSubmit.addEventListener('submit', event => {
+	event.preventDefault();
+	const itemObj = {};
+	for (const elem of elementsModalSubmit) {
+		itemObj[elem.name] = elem.value;
+	};
+	dataBase.push(itemObj);
+});
+
+addAd.addEventListener('click', () => {
+	modalAdd.classList.remove('hide');
+	modalBtnSubmit.disabled = true;
+	document.addEventListener('keydown', closeModalEsc);
+});
+
+catalog.addEventListener('click', event => {
+	const target = event.target;
+
+	if (target.closest('.card')) {
+		modalItem.classList.remove('hide');
+		document.addEventListener('keydown', closeModalEsc);
+	};
+});
+
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
+
+
+
